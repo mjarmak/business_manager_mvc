23f0dd445e5d1ad58f0a9359fa5836e306b9a75d
mjarmak <mohamadjarmak@gmail.com>
2020-06-27 13:06:26 +0200

    added api template and UserAccount.class

---
+++ business_manager_api/UserAccount.cs
@@ -0,0 +1,49 @@
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
+
+namespace business_manager_api
+{
+    public class UserAccount
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        [Index(IsUnique=true)]
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public GenderEnum Gender { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public bool Profession { get; set; }
+
+    }
+    public enum GenderEnum
+    {
+        Male,
+        Female,
+        Other
+    }
+
+    public class UserAccountValidator : AbstractValidator<UserAccount>
+    {
+        private readonly Regex regex = new Regex(@"[^A-Za-z0-9@-_]");
+        private readonly string matchError = "Email cannot contain any special characters";
+        public UserAccountValidator()
+        {
+            RuleFor(x => x.Name).Length(0, 50).NotNull().Matches(regex).WithMessage(matchError);
+            RuleFor(x => x.Surname).Length(0, 50).NotNull().Matches(regex).WithMessage(matchError);
+            RuleFor(x => x.Email).Length(0, 255).NotNull().EmailAddress().Matches(regex).WithMessage(matchError);
+            RuleFor(x => x.Phone).Length(0, 25);
+            RuleFor(x => x.Gender).NotNull();
+            RuleFor(x => x.BirthDate).NotNull();
+            RuleFor(x => x.Profession).NotNull();
+        }
+    }
+}

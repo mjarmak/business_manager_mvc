5b495d9eaee324e1d4e5d28171360ef4ddedf1db
Francesco B <newbreaker@gmail.com>
2020-07-10 18:11:19 +0200

    testing

---
+++ business_manager_api/Controllers/BusinessDataController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using business_manager_api;
+
+namespace business_manager_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusinessDataController : Controller
+    {
+        private readonly DefaultContext _context;
+
+        public BusinessDataController(DefaultContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BusinessData
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BusinessDataModel>>> GetBusinessDataModel()
+        {
+            return await _context.BusinessDataModel.ToListAsync();
+        }
+
+        // GET: api/BusinessData/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BusinessDataModel>> GetBusinessDataModel(long id)
+        {
+            var businessDataModel = await _context.BusinessDataModel.FindAsync(id);
+
+            if (businessDataModel == null)
+            {
+                return NotFound();
+            }
+
+            return businessDataModel;
+        }
+
+        // PUT: api/BusinessData/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBusinessDataModel(long id, BusinessDataModel businessDataModel)
+        {
+            if (id != businessDataModel.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(businessDataModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BusinessDataModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BusinessData
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<BusinessDataModel>> PostBusinessDataModel(BusinessDataModel businessDataModel)
+        {
+            _context.BusinessDataModel.Add(businessDataModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBusinessDataModel", new { id = businessDataModel.Id }, businessDataModel);
+        }
+
+        // DELETE: api/BusinessData/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<BusinessDataModel>> DeleteBusinessDataModel(long id)
+        {
+            var businessDataModel = await _context.BusinessDataModel.FindAsync(id);
+            if (businessDataModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.BusinessDataModel.Remove(businessDataModel);
+            await _context.SaveChangesAsync();
+
+            return businessDataModel;
+        }
+
+        private bool BusinessDataModelExists(long id)
+        {
+            return _context.BusinessDataModel.Any(e => e.Id == id);
+        }
+    }
+}

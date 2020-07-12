4f7d38e5b58ebfea4f576d6095840501a0fd88ab
Francesco B <newbreaker@gmail.com>
2020-07-12 13:38:00 +0200

    tst

---
+++ business_manager_mvc/App_Start/BundleConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace business_manager_mvc.App_Start
+{
+    public class BundleConfig
+    {
+    }
+}
---
+++ business_manager_mvc/ClientApp/src/app/services/alert-service.ts
@@ -0,0 +1,1 @@
+import * as toastr from "toastr";

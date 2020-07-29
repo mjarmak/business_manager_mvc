621da2f30cd47feabfbd9c1fbff175c4c8b8e895
Francesco B <newbreaker@gmail.com>
2020-07-29 19:20:46 +0200

    css

--- business_manager_api/Domain/BusinessDataModel.cs
+++ business_manager_api/Domain/BusinessDataModel.cs
@@ -1,8 +1,9 @@
 ﻿using business_manager_api.Services;
-using business_manager_common_library;
+//using business_manager_common_library;
 using FluentValidation;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.RegularExpressions;
@@ -27,6 +28,7 @@ namespace business_manager_api
         public long Id { get; set; }
         public AddressData Address { get; set; }
         public string Phone { get; set; }
+       
         public string EmailBusiness { get; set; }
         public string UrlSite { get; set; }
         public string UrlInstagram { get; set; }
--- business_manager_api/Domain/UserAccountDataModel.cs
+++ business_manager_api/Domain/UserAccountDataModel.cs
@@ -1,7 +1,6 @@
 using business_manager_api.Services;
 using FluentValidation;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.RegularExpressions;

a226e6219ad7420acdb0045ed3b97bcd44ae2ed5
Francesco B <newbreaker@gmail.com>
2020-07-30 20:44:43 +0200

    remover weather and counter

--- business_manager_mvc/Startup.cs
+++ business_manager_mvc/Startup.cs
@@ -62,16 +62,12 @@ namespace business_manager_mvc
 
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
 
                 spa.Options.SourcePath = "ClientApp";
 
-                if (env.IsDevelopment())
-                {
-                    spa.Options.StartupTimeout = new TimeSpan(0, 2, 80);
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
+                if (!env.IsDevelopment()) return;
+                spa.Options.StartupTimeout = new TimeSpan(0, 2, 80);
+                spa.UseAngularCliServer(npmScript: "start");
             });
         }
     }

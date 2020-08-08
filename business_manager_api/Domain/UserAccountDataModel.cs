48615ce185a750f3aa4db754687fae40c673409c
Francesco B <newbreaker@gmail.com>
2020-08-08 13:37:53 +0200

    fluent validation

--- business_manager_api/Domain/UserAccountDataModel.cs
+++ business_manager_api/Domain/UserAccountDataModel.cs
@@ -24,39 +24,5 @@ namespace business_manager_api
         public string State { get; set; }
     }
 
-    public class UserAccountValidator : AbstractValidator<UserAccountDataModel>
-    {
-        private readonly Regex regex = new Regex(@"[^A-Za-z0-9@-_]");
-        private readonly string matchError = "Cannot contain any special characters";
-        public UserAccountValidator()
-        {
-            RuleFor(x => x.Name)
-                .Length(1, 50).NotNull().WithMessage("The Name field is empty.")
-                .Matches(regex).WithMessage(matchError);
-
-            RuleFor(x => x.Surname)
-                .Length(0, 50).NotNull().WithMessage("The Surname field is empty.")
-                .Matches(regex).WithMessage(matchError);
-
-            RuleFor(x => x.Email)
-                .Length(0, 255).NotNull().WithMessage("The Email field is empty.")
-                .EmailAddress().WithMessage("A valid email is required")
-                .Matches(regex).WithMessage(matchError);
-
-            RuleFor(x => x.Phone)
-                .Length(0, 25);
-
-            RuleFor(x => PhoneNumberCheckView.IsValidPhoneNumber(x.Phone)).Equal(true).WithMessage("Phone number is invalid");
-            RuleFor(x => PhoneNumberCheckView.IsMobileNumber(x.Phone)).Equal(true).WithMessage("Phone number must be a mobile.");
-
-            RuleFor(x => x.Gender)
-                .NotNull().WithMessage("Need to select a gender type.");
-
-            RuleFor(x => x.BirthDate)
-                .NotNull();
-
-            RuleFor(x => x.Profession)
-                .NotNull();
-        }
-    }
+   
 }
